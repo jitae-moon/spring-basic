6ffe23e5fe5339fd2bd320e8273288df8eb71a34
jitae-moon <jitae.moon@icloud.com>
2024-08-01 14:09:42 +0900

    refactor: 상속을 통한 확장
    다른 회사에서 그 회사만의 getExRate()에 대한 로직이 있을 때 상속을 통해 적용할 수 있도록 수정.

---
+++ src/main/java/com/example/springbasic/Client.java
@@ -0,0 +1,14 @@
+package com.example.springbasic;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        PaymentService paymentService = new SimpleExRatePaymentService();
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        System.out.println(payment);
+    }
+
+}
