37bbb2a41c4cf8cbff7f5f33bd1d5d576714f936
jitae-moon <jitae.moon@icloud.com>
2024-08-02 13:50:38 +0900

    refactor: 관계설정 책임의 분리
    `Client`에서 런타임시 사용하는 클래스 지정해줌.

--- src/main/java/com/example/springbasic/Client.java
+++ src/main/java/com/example/springbasic/Client.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
+        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         System.out.println(payment);
     }
