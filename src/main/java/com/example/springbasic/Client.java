4ec1e6afc9345e83ccc1ceb52759a0da6a208bfd
jitae-moon <jitae.moon@icloud.com>
2024-08-02 15:51:09 +0900

    refactor: 설정정보 분리 후 `BeanFactory`를 사용

--- src/main/java/com/example/springbasic/Client.java
+++ src/main/java/com/example/springbasic/Client.java
@@ -1,13 +1,16 @@
 package com.example.springbasic;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         System.out.println(payment);
