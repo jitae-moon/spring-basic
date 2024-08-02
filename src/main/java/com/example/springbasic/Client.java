d0bde9ef65484f4ab04993324d45b27754338319
jitae-moon <jitae.moon@icloud.com>
2024-08-02 14:33:39 +0900

    refactor: `ObjectFactory` 추가
    `Client`에 두 가지 관심사가 혼재되어있음.
    (PaymentService 사용, ExRateProvider 주입)
    의존관계 넣어주는 로직을 `ObjectFactory`로 분리.

--- src/main/java/com/example/springbasic/Client.java
+++ src/main/java/com/example/springbasic/Client.java
@@ -6,7 +6,9 @@ import java.math.BigDecimal;
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
         System.out.println(payment);
     }
---
+++ src/main/java/com/example/springbasic/ObjectFactory.java
@@ -0,0 +1,13 @@
+package com.example.springbasic;
+
+public class ObjectFactory {
+
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+
+}
