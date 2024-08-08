df5da58e6739d97e9c67cfc4a5972e6995fecad8
jitae-moon <jitae.moon@icloud.com>
2024-08-08 14:47:36 +0900

    Template을 별도의 클래스로 분리

--- src/main/java/com/example/springbasic/exrate/WebApiExRateProvider.java
+++ src/main/java/com/example/springbasic/exrate/WebApiExRateProvider.java
@@ -1,49 +1,20 @@
 package com.example.springbasic.exrate;
 
-import com.example.springbasic.api.ApiExecutor;
+import com.example.springbasic.api.ApiTemplate;
 import com.example.springbasic.api.ErApiExRateExtractor;
-import com.example.springbasic.api.ExRateExtractor;
-import com.example.springbasic.api.SimpleApiExecutor;
+import com.example.springbasic.api.HttpClientApiExecutor;
 import com.example.springbasic.payment.ExRateProvider;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebApiExRateProvider implements ExRateProvider {
 
-    // Client
-    // Client가 Callback을 만들어서 Template을 호출
+    private ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
     }
-
-    // Template
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) { // Template & callback
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = apiExecutor.execute(uri);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return exRateExtractor.extract(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
 }
