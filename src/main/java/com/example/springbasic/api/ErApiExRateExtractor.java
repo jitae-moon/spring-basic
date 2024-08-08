7c306ceca1089b7d0786bb7a233412624d0eb4de
jitae-moon <jitae.moon@icloud.com>
2024-08-08 10:27:14 +0900

    `WebApiExRateProvider`내의 변하는 부분 callback으로 분리

---
+++ src/main/java/com/example/springbasic/api/ErApiExRateExtractor.java
@@ -0,0 +1,18 @@
+package com.example.springbasic.api;
+
+import com.example.springbasic.ExRateData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+
+public class ErApiExRateExtractor implements ExRateExtractor {
+
+    @Override
+    public BigDecimal extract(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        return data.rates().get("KRW");
+    }
+
+}
--- src/main/java/com/example/springbasic/exrate/WebApiExRateProvider.java
+++ src/main/java/com/example/springbasic/exrate/WebApiExRateProvider.java
@@ -1,11 +1,11 @@
 package com.example.springbasic.exrate;
 
-import com.example.springbasic.ExRateData;
 import com.example.springbasic.api.ApiExecutor;
+import com.example.springbasic.api.ErApiExRateExtractor;
+import com.example.springbasic.api.ExRateExtractor;
 import com.example.springbasic.api.SimpleApiExecutor;
 import com.example.springbasic.payment.ExRateProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -20,11 +20,11 @@ public class WebApiExRateProvider implements ExRateProvider {
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url, new SimpleApiExecutor());
+        return runApiForExRate(url, new SimpleApiExecutor(), new ErApiExRateExtractor());
     }
 
     // Template
-    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) { // Template & callback
+    private static BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) { // Template & callback
         URI uri;
         try {
             uri = new URI(url);
@@ -40,16 +40,10 @@ public class WebApiExRateProvider implements ExRateProvider {
         }
 
         try {
-            return extractExRate(response);
+            return exRateExtractor.extract(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static BigDecimal extractExRate(String response) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
     }
 
 }
